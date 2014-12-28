161d9ea46bf67e2db833f09d32b98bf339459fdb
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-28 22:37:11 +0000

    Strip PX from root node

---
+++ test/specs/transforms.height.width.js
@@ -0,0 +1,17 @@
+var attrTransforms = require("../../lib/attrTransforms");
+var builder        = require("../../lib/builder");
+var config         = require("../../lib/config");
+var json           = require("../fixtures/objects/arr-black.json");
+
+var assert         = require("chai").assert;
+var path           = require("path");
+
+describe("Transforming height and width attributes", function () {
+    it("strips px", function () {
+        var out = builder.doTransformsOnNode(json.svg.$, attrTransforms, {path: "/Users/shane/file-1.svg"}, config());
+        assert.deepEqual(out.x, 0);
+        assert.deepEqual(out.y, 0);
+        assert.deepEqual(out.width,  8);
+        assert.deepEqual(out.height, 12);
+    });
+});
